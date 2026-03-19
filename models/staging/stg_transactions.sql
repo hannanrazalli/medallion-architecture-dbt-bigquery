b01acd2163a48b35bb17e065e088a570f642d579
Abdullah Hannan Razalli <a.hannanrazalli@gmail.com>
2026-03-19 15:13:13 +0000

    Practice stg

--- models/staging/stg_transactions.sql
+++ models/staging/stg_transactions.sql
@@ -1,10 +1,9 @@
 {{ config(
-    materialized='incremental',
+    materialized='incremental', 
     unique_key='txn_id'
 ) }}
 
 with raw_data as (
-    -- Pastikan nama source ni SAMA macam dalam src_transactions.yml kau
     select * from {{ source('transactions_source', 'transactions_batch_1') }}
 )
 
@@ -12,18 +11,10 @@ select
     txn_id,
     cust_id,
     amount,
-    is_member,
     points,
+    is_member,
     status,
     txn_date,
 
-    {{ audit_columns('bronze') }},
-    'CLEAN' as _record_status,
-    cast(null as string) as _source_file
-
-from raw_data
-
-{% if is_incremental() %}
-  -- Logic incremental untuk elak duplicate batch
-  where txn_date > (select max(txn_date) from {{ this }})
-{% endif %}
+    case
+        when txn_id is null or 
