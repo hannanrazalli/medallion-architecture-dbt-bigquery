a9e19ad0fbf886e5131d4911e6c5cadd24856004
Abdullah Hannan Razalli <a.hannanrazalli@gmail.com>
2026-03-26 15:18:51 +0000

    Update int_transactions_refined

--- macros/generate_audit_columns.sql
+++ macros/generate_audit_columns.sql
@@ -1,13 +1,13 @@
 {% macro audit_columns(layer) %}
     {% if layer == 'bronze' %}
-        current_timestamp() AS _ingest_at,
-        current_date AS _ingest_date,
+        current_timestamp()  AS _ingest_at,
+        current_date() AS _ingest_date,
         '{{ invocation_id }}' AS _batch_id_bronze
     {% elif layer == 'silver' %}
-        current_timestamp() AS _processed_at,
+        current_timestamp()  AS _processed_at,
         '{{ invocation_id }}' AS _batch_id_silver
     {% elif layer == 'gold' %}
-        current_timestamp() AS _processed_at_gold,
+        current_timestamp()  AS _processed_at_gold,
         '{{ invocation_id }}' AS _batch_id_gold
     {% endif %}
 {% endmacro %}
