4376baa04e3ad01fd2bddc356dcf6886c40e47b5
Abdullah Hannan Razalli <a.hannanrazalli@gmail.com>
2026-03-28 03:57:25 +0000

    Practice dim_customers

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -5,62 +5,58 @@
     on_schema_change='append_new_columns'
 ) }}
 
--- CTE 1: Ambil data baru dari Silver (Incremental logic)
-with source_data as (
-    select
+WITH source_data AS (
+    SELECT
         cust_id,
         is_member,
-        _processed_at as valid_from
-    from {{ ref('int_transactions_refined') }}
+        _processed_at AS valid_from
+    FROM {{ ref('int_transactions_refined') }}
     {% if is_incremental() %}
-      -- Watermark logic untuk jimat kos BigQuery
-      where _processed_at > (select max(valid_from) from {{ this }})
+      WHERE _processed_at > (
+        SELECT max(valid_from)
+        FROM {{ this }}
+      )
     {% endif %}
 ),
 
--- CTE 2: Deduplikasi (Grain of Truth - 1 row per customer per batch)
-deduplicated as (
-    select *
-    from source_data
-    qualify row_number() over (
-        partition by cust_id 
+deduplicate AS (
+    SELECT *
+    FROM source_data
+    qualify row_number() over(
+        partition by cust_id
         order by valid_from desc
     ) = 1
 ),
 
--- CTE 3: Prepare record baru (SCD 2 - New/Current Record)
-final_staged as (
-    select
-        {{ dbt_utils.generate_surrogate_key(['cust_id', 'is_member']) }} as hash_key,
+final_staged AS (
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['cust_id', 'is_member']) }} AS hash_key,
         cust_id,
         is_member,
         valid_from,
-        cast(null as timestamp) as valid_to,
+        cast(NULL as timestamp) AS valid_to,
         true as is_current,
-        -- Panggil macro dengan string 'gold'
         {{ audit_columns('gold') }}
-    from deduplicated
+    FROM deduplicate
 )
 
--- FINAL OUTPUT
-select * from final_staged
+SELECT *
+FROM final_staged
 
 {% if is_incremental() %}
 union all
 
--- PRO LOGIC: "Expire-kan" record lama kalau ada perubahan hash_key
-select
+SELECT
     t.hash_key,
     t.cust_id,
     t.is_member,
     t.valid_from,
-    s.valid_from as valid_to, 
-    false as is_current,
-    -- Pastikan panggil macro yang sama untuk struktur column yang sama
+    s.valid_from AS valid_to,
+    false AS is_current,
     {{ audit_columns('gold') }}
-from {{ this }} t
-inner join final_staged s 
-    on t.cust_id = s.cust_id
-where t.is_current = true
-  and t.hash_key <> s.hash_key 
+FROM {{ this }} t
+INNER JOIN final_staged s
+    ON t.cust_id = s._cust_id
+WHERE t.is_current = true
+  AND t.hash_key <> s.hash_key
 {% endif %}
