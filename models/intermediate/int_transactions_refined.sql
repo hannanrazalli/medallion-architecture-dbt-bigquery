d8b99895293d53af43815fba461d6fb9daac96bb
Abdullah Hannan Razalli <a.hannanrazalli@gmail.com>
2026-04-02 00:21:31 +0000

    Removed materialized

--- models/intermediate/int_transactions_refined.sql
+++ models/intermediate/int_transactions_refined.sql
@@ -1,16 +1,17 @@
 {{ config(
-    materialized='incremental',
-    unique_key='txn_id',
+    unique_key='hash_key',
     incremental_strategy='merge'
 ) }}
 
 WITH source_data AS (
     SELECT *
     FROM {{ ref('stg_transactions') }}
-    WHERE _ingest_at > (
-        SELECT timestamp_sub(max(_ingest_at), INTERVAL 1 HOUR)
-        FROM {{ this }}
-    )
+    {% if is_incremental() %}
+        WHERE _ingest_at > (
+            SELECT timestamp_sub(max(_ingest_at), INTERVAL 1 HOUR)
+            FROM {{ this }}
+        )
+    {% endif %}
 ),
 
 deduplicate AS (
@@ -30,14 +31,14 @@ transformed AS (
         points,
         is_member,
         CASE
-            WHEN upper(trim(status)) IN ('COMPLETED', 'CANCELLED', 'PENDING') THEN upper(trim(status))
+            WHEN upper(trim(status)) IN ('COMPLETED', 'PENDING', 'CANCELLED') THEN upper(trim(status))
             ELSE 'UNKNOWN'
         END AS status,
         txn_date,
-        (upper(trim(status)) = 'CANCELLED') AS is_deleted,
         _record_status,
         _ingest_at,
         _batch_id_bronze,
+        (upper(trim(status)) = 'CANCELLED') AS _is_deleted,
         {{ audit_columns('silver') }}
     FROM deduplicate
 )
